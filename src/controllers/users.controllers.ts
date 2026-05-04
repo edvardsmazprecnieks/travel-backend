e598e320ab4fc45559978adaf82d2147cf122644
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2026-05-04 07:20:13 +0200

    testing cookie

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -54,10 +54,10 @@ export const loginUserController = async (req: Request, res: Response): Promise<
         await userServices.saveRefreshToken(loggedInUser.id, refreshToken);
 
         res.cookie('refreshToken', refreshToken, {
-            domain: 'https://travelapp-frontend-statid.onrender.com/',
+            domain: 'travelapp-frontend-statid.onrender.com',
             httpOnly: true,
             secure: true,
-            sameSite: 'lax',
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
             path: '/',
         });

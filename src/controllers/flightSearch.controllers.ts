0decc1ff3f962f701103ef536cfbecf1eeba6fe9
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2026-05-11 22:28:46 +0200

    add zod to flightSearch controller

--- src/controllers/flightSearch.controllers.ts
+++ src/controllers/flightSearch.controllers.ts
@@ -1,39 +1,38 @@
 import type { Request, Response } from 'express';
 import * as flightSearchServices from '../services/flightSearch.services.js';
 import * as Sentry from '@sentry/node';
+import { z } from 'zod';
+
+const flightSearchObject = z.object({
+    originLocationCode: z.string().regex(/^[a-zA-Z]{3}$/, 'Invalid IATA code'),
+    destinationLocationCode: z.string().regex(/^[a-zA-Z]{3}$/, 'Invalid IATA code'),
+    departureDate: z.iso.date('Invalid date'),
+    adults: z
+        .string()
+        .transform(Number)
+        .pipe(z.number().int().positive('Number of passengers incorrect')),
+});
 
 export const searchFlightsController = async (req: Request, res: Response): Promise<void> => {
+    const parseData = flightSearchObject.safeParse(req.query);
+
+    if (!parseData.success) {
+        res.status(400).json({
+            message: 'Missing or invalid search parameters.',
+            errors: z.treeifyError(parseData.error),
+        });
+        return;
+    }
+
     try {
-        const { originLocationCode, destinationLocationCode, departureDate, adults } = req.query;
-
-        const iataCodeRegex = /^[a-zA-Z]{3}$/;
-        const travelDateRegex = /^\d{4}-\d{2}-\d{2}$/;
-
-        if (
-            typeof originLocationCode !== 'string' ||
-            typeof destinationLocationCode !== 'string' ||
-            typeof departureDate !== 'string' ||
-            typeof adults !== 'string' ||
-            !iataCodeRegex.test(originLocationCode) ||
-            !iataCodeRegex.test(destinationLocationCode) ||
-            !travelDateRegex.test(departureDate)
-        ) {
-            res.status(400).json({ message: 'Missing or invalid required search parameters.' });
-            return;
-        }
-
-        const numberOfAdults = Number(adults);
-
-        if (!Number.isInteger(numberOfAdults) || numberOfAdults <= 0) {
-            res.status(400).json({ message: 'Number of passengers is incorrect.' });
-            return;
-        }
+        const { originLocationCode, destinationLocationCode, departureDate, adults } =
+            parseData.data;
 
         const flightOffers = await flightSearchServices.findFlightOffers({
             originLocationCode,
             destinationLocationCode,
             departureDate,
-            adults: numberOfAdults,
+            adults,
         });
 
         res.status(200).json(flightOffers);

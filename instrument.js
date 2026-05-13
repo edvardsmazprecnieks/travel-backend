869cfb28e841381fae57b3840ad807cff891f16a
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2026-05-13 09:54:32 +0200

    Sentry DSN security fix

--- instrument.js
+++ instrument.js
@@ -1,6 +1,7 @@
+import 'dotenv/config';
 import * as Sentry from '@sentry/node';
 
 Sentry.init({
-    dsn: 'https://72909e5eb8a41b35de1295741cabfcd8@o4511327540084736.ingest.de.sentry.io/4511327545131088',
-    sendDefaultPii: true,
+    dsn: process.env.SENTRY_DSN,
+    sendDefaultPii: false,
 });
